8a0e33f62df3fa3b0d5f8ee47fe7ac6b25072a0d
liuyuelintop <liuyuelintop@gmail.com>
2018-07-20 09:59:41 +0800

    users can see their orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,10 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
-    @order = Order.new(order_params)
-    @order.user = current_user
+    @order = current_user.orders.build
     @order.total = current_cart.total_price
-
     if @order.save
+
       current_cart.cart_items.each do |cart_item|
         product_list = ProductList.new
         product_list.order = @order
@@ -17,14 +16,16 @@ class OrdersController < ApplicationController
       end
 
       redirect_to order_path(@order.token)
+
     else
       render 'carts/checkout'
     end
   end
 
+
   def show
-    @order = Order.find(params[:id])
     @order = Order.find_by_token(params[:id])
+    @product_lists = @order.product_lists
   end
 
   private
@@ -32,4 +33,5 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:billing_name, :billing_address, :shipping_name, :shipping_address)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
        post :checkout
     end
   end
-  resources :cart_items
-  resources :orders
+ namespace :account do
+   resources :orders
+ end
+ resources :cart_items
+ resources :orders
+
 end
